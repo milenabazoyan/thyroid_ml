e921cfd2965b37411f9bed1f61727ce61f3cbe78
milenabazoyan <92885934+milenabazoyan@users.noreply.github.com>
2022-07-27 14:10:21 +0400

    Add files via upload

---
+++ thyroid_ml/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='added project structure with cookiecutter',
+    author='Milena ',
+    license='MIT',
+)
