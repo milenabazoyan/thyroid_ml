eb9d837ae58e48c9ddd6b45fac6f22b5f973d543
Zaven Zareyan <zzareyan@amplify.com>
2022-07-28 16:01:54 +0400

    corrected build features

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,9 +1,7 @@
-from pathlib import Path
 from typing import Tuple
 
 import pandas as pd
 from numpy import nan
-from data import raw, processed
 
 
 def read_names(path) -> Tuple[list, list, list]:
@@ -25,43 +23,44 @@ def read_names(path) -> Tuple[list, list, list]:
     return names, numeric, boolean
 
 
-def transform(input_file: Path, binary=False) -> None:
+def transform(input_file: str, binary=False) -> None: # typehints
     # Reading and parsing column names from naming file
-    names, numeric, boolean = read_names(raw)
+    names, numeric, boolean = read_names(input_file)
 
     # Loading raw df
-    df = pd.read_csv(raw, header=None)
+    df = pd.read_csv(input_file, header=None)
 
     if names:
         df.columns = names
 
+    df = df.drop(['TBG', 'referral source'], axis=1)
+
     # Removing bad values ('?'), converting other binary values (not boolean)
     df = df.replace({"?": nan})
+
+    # Handle missing values / listwise deletion
+    df = df.dropna().reset_index()
+
     df['sex'] = df['sex'].replace({'F': 1, 'M': 0}).astype(int)
 
     # Cleaning 'diagnosis' column
     df['diagnosis'] = df['diagnosis'].apply(lambda x: "negative" if x.split('.')[0] == "negative" else "positive") \
         if binary else df['diagnosis'].apply(lambda x: x.split('.')[0])
 
-    # Converting numeric columns
-    for col_name in df.columns:
-        df[col_name] = df[col_name].apply(pd.to_numeric, errors='coerce')
-
     # Converting boolean columns
     for col_name in boolean:
         df[col_name] = df[col_name].replace({"t": 1, "f": 0}).astype(int)
 
-    df = df.drop(['TBG', 'referral source'], axis=1)
-
-    # Handle missing values
-    df = df.dropna().reset_index()
+    # Converting numeric columns
+    for col_name in df.columns:
+        df[col_name] = df[col_name].apply(pd.to_numeric, errors='coerce')
 
     # Saving cleaned df
-    df.to_feather(processed)
+    df.to_feather(input_file.replace("data/raw", "data/processed"))
 
 
 if __name__ == '__main__':
     # For train data
-    transform("allhypo.data")
+    transform("../../data/raw/allhypo.data", binary=True)
     # For test data
-    transform("allhypo.test")
+    transform("../../data/raw/allhypo.test", binary=True)
