1e284f18ce8687f28aaf6d354c666e89eccc5716
milenabazoyan <92885934+milenabazoyan@users.noreply.github.com>
2022-08-07 22:23:37 +0400

    Add files via upload

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,3 +4,24 @@
 # 1. split processed dataframe to x_train and y_train
 # 2. use KNN Classifier to fit and get model, use https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
 # 3. dump model to models/{datetime now}/knn.bin, use "from joblib import dump"
+
+from sklearn.neighbors import KNeighborsClassifier
+from joblib import dump
+import datetime
+
+def get_model(df):
+
+    X_train = df.iloc[:, :-1]
+    Y_train = df['diagnosis']
+
+    knn_model = KNeighborsClassifier(n_neighbors=3)
+    knn_model.fit(X_train, Y_train)
+
+    predictiton = knn_model.predict(X_train)
+
+    x = datetime.datetime.now()
+
+    dump(knn_model, '../models/x')
+
+
+
