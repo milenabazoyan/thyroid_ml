c1499cc3645aa19ecfc35cf70becff7d08c95964
Zaven Zareyan <zzareyan@amplify.com>
2022-08-11 14:24:24 +0400

    added notes

---
+++ src/models/predict_model.py
@@ -0,0 +1,6 @@
+# 1. Typehints
+# 2. separate x_test, y_test
+# 3. load model from joblib.load and run model.predict on x_test
+# 4. see the differences of y_test and model predicted values
+# 5. change neighbors to 15 and see if our model is better
+# 6. read something about metrics
