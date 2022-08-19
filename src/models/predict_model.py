894d681d731b2c5141e8f4403dd09f9972b5c228
milenabazoyan <92885934+milenabazoyan@users.noreply.github.com>
2022-08-19 19:14:30 +0400

    Update predict_model.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -4,3 +4,13 @@
 # 4. see the differences of y_test and model predicted values
 # 5. change neighbors to 15 and see if our model is better
 # 6. read something about metrics
+
+
+import joblib
+
+def predict(model):
+    X_test = model.iloc[:, :-1].values
+    Y_test = model['diagnosis'].values
+
+    loaded_model = joblib.load(model)
+    prdct = loaded_model.predict(X_test)
