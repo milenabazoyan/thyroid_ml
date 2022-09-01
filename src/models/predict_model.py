84c154c1fd0878bbd2a24f30c532eb94764b8b5b
Zaven Zareyan <zzareyan@amplify.com>
2022-09-01 15:56:34 +0400

    fixed paths and added metrics

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -7,11 +7,22 @@
 
 
 import joblib
+from sklearn.metrics import confusion_matrix, balanced_accuracy_score, accuracy_score
+
 
 def predict(test_data):
     X_test = test_data.iloc[:, :-1].values
     Y_test = test_data['diagnosis'].values
 
-    model_path = "models/08_30_2022_13_50_00/knn.bin"
+    model_path = "models/09_01_2022_15_38_53/knn.bin"
     loaded_model = joblib.load(model_path)
-    prdct = loaded_model.predict(X_test)
+    predicted_values = loaded_model.predict(X_test)
+    scores = loaded_model.predict_proba(X_test)
+
+    cm = confusion_matrix(Y_test, predicted_values)
+    ba = balanced_accuracy_score(Y_test, predicted_values)
+    # a = accuracy_score(Y_test, predicted_values)
+
+    # f1 score, matthews_corrcoef and other metrics
+    print(predicted_values)
+    print(scores)
--- tests/test_predict_model.py
+++ tests/test_predict_model.py
@@ -2,6 +2,6 @@ import feather
 
 from src.models.predict_model import predict
 
-test_data = feather.read_dataframe("../../data/processed/allhypo.test")
+test_data = feather.read_dataframe("data/processed/allhypo.test")
 
 predict(test_data)
